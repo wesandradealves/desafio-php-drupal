e054dead187f30233158ceee88231854787d35fd
Wesley Alves <desenvolvimento2@cityconnect.com.br>
2024-04-02 05:58:53 -0300

    Copyright; Scroll up;

--- themes/custom/khipo/assets/js/scripts.js
+++ themes/custom/khipo/assets/js/scripts.js
@@ -7,6 +7,11 @@
 ;(function ($, Drupal) {
   'use strict'
   jQuery(document).ready(function () {
-      
+    $('[rel="top"]').click(function (e) {
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    })
   })
 })(jQuery, Drupal)
